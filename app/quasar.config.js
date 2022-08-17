6fd0f5d8f9e69262491323afb4ed724314fd5aa5
polectron <polectron@gmail.com>
2022-08-17 23:43:04 +0200

    run app on subfolder

--- app/quasar.config.js
+++ app/quasar.config.js
@@ -67,7 +67,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: 'OpenScan3-Client',
       // analyze: true,
       // env: {},
       // rawDefine: {}
