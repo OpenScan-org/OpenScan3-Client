c6b8e50ca51fd2b9559f36a2c828103c4c4eb17a
polectron <polectron@gmail.com>
2022-08-17 23:48:51 +0200

    remove gh-pages deploy

--- app/quasar.config.js
+++ app/quasar.config.js
@@ -67,7 +67,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: 'OpenScan3-Client',
+      // publicPath: '/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
