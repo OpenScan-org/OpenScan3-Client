07983f0a5bb0c8d88356f347bf7b486dfb488424
polectron <polectron@gmail.com>
2022-08-19 00:31:34 +0200

    add settings route

---
+++ app/src/pages/SettingsPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <q-page>
+        <div class="q-pa-md">
+            <div class="row justify-center">
+                <div class="col-8">
+                    <q-card class="project-card">
+                        <q-card-section>
+                            <div class="text-h6">Scanner Settings</div>
+                        </q-card-section>
+                        <q-card-section>
+                            ...
+                        </q-card-section>
+                        <q-separator inset />
+                        <q-card-section align="right">
+                            <q-btn color="primary" icon-right="send" label="Save" type="submit"/>
+                        </q-card-section>
+                    </q-card>
+                </div>
+            </div>
+        </div>
+    </q-page>
+</template>
