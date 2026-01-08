f63e0d44740b59bb87899ac5c774b37c3daaf2a1
esto-openscan <193278706+esto-openscan@users.noreply.github.com>
2026-01-08 11:32:41 +0100

    add go to projects after scan is finished

--- app/src/components/scan/RunningScanProgress.vue
+++ app/src/components/scan/RunningScanProgress.vue
@@ -31,6 +31,12 @@
           color="negative"
           @click="cancel"
         />
+        <BaseButtonPrimary
+          v-if="task.status === 'completed' || task.status === 'cancelled' || task.status === 'error'"
+          icon="folder_open"
+          label="Go to Project"
+          @click="goToProject"
+        />
         <BaseButtonSecondary
           v-if="task.status === 'completed' || task.status === 'cancelled' || task.status === 'error'"
           icon="close"
@@ -44,10 +50,12 @@
 
 <script setup lang="ts">
 import { computed, onBeforeUnmount, onMounted, ref, watch } from 'vue'
+import { useRouter } from 'vue-router'
 import type { Task } from 'src/generated/api'
 import BaseButtonSecondary from 'components/base/BaseButtonSecondary.vue'
 import BaseSection from 'components/base/BaseSection.vue'
 import { useTaskStore } from 'src/stores/tasks'
+import BaseButtonPrimary from "components/base/BaseButtonPrimary.vue";
 
 const props = defineProps<{
   taskId: string
@@ -59,6 +67,7 @@ const emit = defineEmits<{
 }>()
 
 const taskStore = useTaskStore()
+const router = useRouter()
 void taskStore.ensureConnected()
 
 const task = computed(() => (taskStore.taskById(props.taskId) ?? props.initialTask)!)
@@ -140,6 +149,13 @@ const cancel = async () => {
 
 const close = () => {
   emit('close')
+}
+
+const goToProject = () => {
+  void router.push({
+    path: '/projects',
+    query: { project: scanProjectName.value }
+  })
 }
 
 const ensureTaskLoaded = async () => {
